b19034758ebca8e8bd18a317142a0065dd7d7b69
Aman Shrestha <ashrest2@u.rochester.edu>
2021-04-24 14:44:14 -0400

    changed color of finger

--- public/js/main.js
+++ public/js/main.js
@@ -4,10 +4,14 @@ $(document).ready(()=>{
     var canvasCtx = canvasElement.getContext('2d');
     console.log(canvasCtx);
 
+    var xPrev = -1;
+    var yPrev = -1;
+    var drawOn = false;
+
 
     var previousCanvas = canvasCtx.getImageData(0, 0, 1280, 720);
 
-    const acceptedDistance = 10;
+    const acceptedDistance = 15;
     
     function onResults(results) {
       canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
@@ -22,24 +26,50 @@ $(document).ready(()=>{
                 + 
                 Math.pow((indexFinger[1]-thumb[1]), 2) 
             );
+
+            var normalColor = "#999999";
+            var drawingColor = "#33cc99";
             if(distance<acceptedDistance){
                 startDrawing(indexFinger[0],indexFinger[1]);
+                normalColor = drawingColor;
+            }else{
+              drawOn = false;
             }
             previousCanvas = canvasCtx.getImageData(0, 0, 1280, 720)
-            // previousCanvas = convertCanvasToImage(canvasElement);
+            canvasCtx.save();
+            canvasCtx.fillStyle = normalColor;
             canvasCtx.fillRect(indexFinger[0],indexFinger[1],15,15);
             canvasCtx.fillRect(thumb[0],thumb[1],15,15);
+            canvasCtx.restore();
+            // canvasCtx.putImageData(previousCanvas,0,0);
         }
       }
 
     }
 
     function startDrawing(x, y){
-      canvasCtx.fillRect(x,y,15,15);
+      console.log("Drawing");
+      if(!drawOn){
+        drawOn = true;
+        canvasCtx.fillRect(x,y,15,15);
+      }else{
+        canvasCtx.fillRect(x,y,15,15);
+        roundLine(x,y,xPrev,yPrev);
+      }
       xPrev = x;
       yPrev = y;
       
+    }
 
+    function roundLine(startX, startY, endX, endY){
+      var dx = endX - startX;
+      var dy = endY - startY;
+      var distance = Math.max(Math.abs(dx), Math.abs(dy));
+      for(var i =0;i<distance;i++){
+        var x = startX + (i+0.0)/distance * dx;
+        var y = startY + (i+0.0)/distance * dy;
+        canvasCtx.fillRect(x,y,15,15);
+      }
     }
     
     const hands = new Hands({locateFile: (file) => {
@@ -47,15 +77,14 @@ $(document).ready(()=>{
     }});
     hands.setOptions({
       maxNumHands: 2,
-      minDetectionConfidence: 0.8,
-      minTrackingConfidence: 0.8
+      minDetectionConfidence: 0.9,
+      minTrackingConfidence: 0.9
     });
     hands.onResults(onResults);
     
     const camera = new Camera(videoElement, {
       onFrame: async () => {
         await hands.send({image: videoElement});
-
       },
       width: 1280,
       height: 720
--- src/Components/CameraCode.js
+++ src/Components/CameraCode.js
@@ -6,7 +6,7 @@ export default function CameraCode() {
     return (
     <div className="container">
         <canvas className="output_canvas" width="1280px" height="720px"></canvas>
-        <video className="input_video"></video>
+        <video className="input_video" width="100%"></video>
     </div>
   
     )
