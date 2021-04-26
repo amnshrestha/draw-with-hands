1d15a7d13776f0d543da47d80cfc5aaca08b36cd
Aman Shrestha <ashrest2@u.rochester.edu>
2021-04-26 16:59:52 -0400

    Updates

---
+++ js/main.js
@@ -0,0 +1,149 @@
+$(document).ready(()=>{
+    const videoElement = document.getElementsByClassName('input_video')[0];
+    const canvasElement = document.getElementsByClassName('output_canvas')[0];
+    var canvasCtx = canvasElement.getContext('2d');
+
+    var radius = 15;
+    var xPrev = -1;
+    var yPrev = -1;
+    var drawOn = false;
+    var drawingColor = '#000000';
+    canvasCtx.fillStyle = drawingColor;
+
+
+    var previousCanvas = canvasCtx.getImageData(0, 0, 1280, 720);
+
+    const acceptedDistance = 15;
+    
+    function onResults(results) {
+      canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+      canvasCtx.putImageData(previousCanvas,0,0);
+      if (results.multiHandLandmarks) {
+        for (const landmarks of results.multiHandLandmarks) {
+            var indexFinger = findCoordinates(landmarks,4);
+            var thumb = findCoordinates(landmarks,8);
+            //distance Left Lip To Left Corner
+            var distance = Math.sqrt( 
+                Math.pow((indexFinger[0]-thumb[0]), 2) 
+                + 
+                Math.pow((indexFinger[1]-thumb[1]), 2) 
+            );
+
+            var normalColor = "#999999";
+            var drawingColor = "#33cc99";
+            var isDrawing = false;
+            if(distance<acceptedDistance){
+                startDrawing(indexFinger[0],indexFinger[1]);
+                normalColor = drawingColor;
+                isDrawing = true;
+            }else{
+              drawOn = false;
+            }
+            previousCanvas = canvasCtx.getImageData(0, 0, 1280, 720)
+            canvasCtx.save();
+            canvasCtx.fillStyle = normalColor;
+            canvasCtx.beginPath();
+            canvasCtx.arc(indexFinger[0],indexFinger[1],radius,0,Math.PI*2);
+            if(!isDrawing)canvasCtx.stroke();
+            else canvasCtx.fill();
+            canvasCtx.beginPath();
+            canvasCtx.arc(thumb[0],thumb[1],radius,0,Math.PI*2);
+            if(!isDrawing)canvasCtx.stroke();
+            else canvasCtx.fill();
+            canvasCtx.restore();
+        }
+      }
+
+    }
+
+    function startDrawing(x, y){
+      console.log("Drawing");
+      canvasCtx.save();
+      canvasCtx.beginPath();
+      canvasCtx.arc(x,y,radius,0,Math.PI*2);
+      canvasCtx.fill();
+      canvasCtx.restore();
+      if(!drawOn){
+        drawOn = true;
+      }else{
+        roundLine(x,y,xPrev,yPrev);
+      }
+      xPrev = x;
+      yPrev = y;
+      
+    }
+
+    function roundLine(startX, startY, endX, endY){
+      var dx = endX - startX;
+      var dy = endY - startY;
+      var distance = Math.max(Math.abs(dx), Math.abs(dy));
+      for(var i =0;i<distance;i++){
+        var x = startX + (i+0.0)/distance * dx;
+        var y = startY + (i+0.0)/distance * dy;
+        canvasCtx.save();
+        canvasCtx.beginPath();
+        canvasCtx.arc(x,y,radius,0,Math.PI*2);
+        canvasCtx.fill();
+        canvasCtx.restore();
+      }
+    }
+    
+    const hands = new Hands({locateFile: (file) => {
+      return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
+    }});
+    hands.setOptions({
+      maxNumHands: 2,
+      minDetectionConfidence: 0.95,
+      minTrackingConfidence: 0.95
+    });
+    hands.onResults(onResults);
+    
+    const camera = new Camera(videoElement, {
+      onFrame: async () => {
+        await hands.send({image: videoElement});
+      },
+      width: 1280,
+      height: 720
+    });
+    camera.start();
+
+
+    // Written by Aman
+    // Finds x,y coordinate of a landmark
+    function findCoordinates(toEnumerate, idToFind){
+        var width = canvasElement.width;
+        var height = canvasElement.height;
+        var cx = parseInt(toEnumerate[idToFind].x * width);
+        var cy = parseInt(toEnumerate[idToFind].y * height);
+        return [cx, cy];
+    }
+
+
+    $('#clearScreen').click(()=>{
+      canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+      previousCanvas = canvasCtx.getImageData(0, 0, 1280, 720)
+    });
+
+
+    $('#utensilSelect').change(function(){ 
+      var value = $(this).val();
+      if(value ==='brush'){
+        canvasCtx.fillStyle = drawingColor;
+      }else if( value === 'eraser'){
+        canvasCtx.fillStyle = '#ffffff';
+      }
+    });
+    
+    $('#brushSizeSelect').change(function(){ 
+      var value = $(this).val();
+      radius = value;
+    });
+    $('#colorSelect').change(function(){ 
+      var value = $(this).val();
+      drawingColor = value;
+      canvasCtx.fillStyle = value;
+    });
+
+    
+});
+
